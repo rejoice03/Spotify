63e430eab0d99c0d740494c2baf9fb2695673af6
Rejoice Raju <rejoiceraju03@gmail.com>
2024-12-16 14:23:51 +0530

    update

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log('Lets write JavaScript');
+"http://127.0.0.1:3000/songs/"
+
+// async function main() {
+//     let a = await fetch('http://127.0.0.1:3000/songs/')
+//     let response = await a.text();
+//     console.log(response);
+//     let div = document.createElement("div")
+//     div.innerHTML = response;
+//     let tds = div.getElementsByTagName("td")
+//     console.log(tds)
+// }
+
+// main()
+// async function main() {
+    
+//     let a = await fetch("http://127.0.0.1:3000/songs/")
+//     let response = await a.text();
+//     console.log(response)
+// }
+// main()
